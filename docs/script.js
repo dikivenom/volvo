7c1208e14dd577c180bf4090a5b19bf923d47666
dikivenom <dikivenom@gmail.com>
2024-10-29 19:30:23 +0700

    add new features

---
+++ docs/script.js
@@ -0,0 +1,27 @@
+const allTab = document.querySelectorAll('.tab');
+
+allTab.forEach(tab => {
+  tab.addEventListener('click', e => {
+    e.preventDefault()
+    const carElement = tab.closest('.grid');
+    const pricingContent = carElement.querySelector('.pricingContent');
+    const infoContent = carElement.querySelector('.infoContent');
+
+    const tabPricing = carElement.querySelector('.pricingTab');
+    const tabInfo = carElement.querySelector('.infoTab');
+
+    if(tab === tabPricing){
+      pricingContent.classList.remove('hidden');
+      infoContent.classList.add('hidden');
+      tabPricing.classList.add('border-b-2', 'border-[#001B72]');
+      tabInfo.classList.remove('border-b-2', 'border-[#001B72]');
+
+    } else if(tab.classList.contains('infoTab')){
+      pricingContent.classList.add('hidden');
+      infoContent.classList.remove('hidden');
+      tabPricing.classList.remove('border-b-2', 'border-[#001B72]');
+      tabInfo.classList.add('border-b-2', 'border-[#001B72]');
+    }
+
+  })
+})
--- script.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.getElementById('menu');
-const listMenu = document.getElementById('list');
-menuButton.addEventListener('click', function(){
-    listMenu.classList.toggle('hidden');
-})
