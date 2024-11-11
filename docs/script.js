d249ef9a6f9b0bc9869ee120ebf1606cf2a11c65
dikivenom <dikivenom@gmail.com>
2024-11-11 16:13:53 +0700

    feat: responsive for medium screen

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,33 @@
 const allTabs = document.querySelectorAll('.tab');
 
 allTabs.forEach(tabs => {
-
   tabs.addEventListener('click', e => {
     e.preventDefault();
-    const carElements = tabs.closest('.grid');
-    const pricingContent = carElements.querySelector('.pricingContent');
-    const infoContent = carElements.querySelector('.infoContent');
     
-    const tabPricing = carElements.querySelector('.pricingTab');
-    const tabInfo = carElements.querySelector('.infoTab');
-  
+    const carElement = tabs.closest('.grid');
+    const tabPricing = carElement.querySelector('.pricingTab');
+    const tabInfo = carElement.querySelector('.infoTab');
+    
+
+    const pricingContent = carElement.querySelector('.pricingContent');
+    const infoContent = carElement.querySelector('.infoContent');
+    const contentBox = carElement.querySelector('.content-box')
+
     if (tabs === tabPricing){
       tabPricing.classList.add('border-b-2', 'border-[#001B72]');
-      pricingContent.classList.remove('hidden');
       tabInfo.classList.remove('border-b-2', 'border-[#001B72]');
+      pricingContent.classList.remove('hidden');
       infoContent.classList.add('hidden');
-    } else if (tabs === tabInfo){
+      contentBox.classList.remove('md:mt-0', 'md:pt-1');
+    }else if (tabs === tabInfo){
       tabInfo.classList.add('border-b-2', 'border-[#001B72]');
-      infoContent.classList.remove('hidden');
       tabPricing.classList.remove('border-b-2', 'border-[#001B72]');
+      infoContent.classList.remove('hidden');
       pricingContent.classList.add('hidden');
+      contentBox.classList.add('md:mt-0', 'md:pt-1');
+    
     }
-  
+
   })
-  })
- 
+
+})
